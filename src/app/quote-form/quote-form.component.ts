c60da718fd57377debfe976937fc3503b9d0218a
Eddie956 <mutugieddie3@gmail.com>
2018-04-22 08:32:37 +0300

    this will commit package-lock.json,package.json,src/app/app.component.css, src/app/quote-form/quote-form.component,css, src/app/quote-form/quote-form.component.html,src/app/quote-form/quote-form.component.ts,src/app/quote.ts,src/app/quote/quote.component.css,src/app/quote/quote.component.html,src/app/quote/quote.component.ts

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter} from '@angular/core';
+import  {Quote} from "../quote"
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  @Output() emitQuote= new EventEmitter(); 
+  publisher:string
+  quote:string
+  quoteAuthor: string
+  myQuote:any
+
+  submitQuote(){
+    this.myQuote = new Quote(this.quote,this.publisher,this.quoteAuthor)
+    this.emitQuote.emit(this.myQuote)
+  } 
+  
+  btnSubmit: string = ' submit';
 
   constructor() { }
 
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,17 +1,11 @@
 export class Quote {
-    
-    public quote:string;
     public author:string;
     public upvote:number;
     public downvote: number;
-    public date:Date;
-    constructor(id,quote, author, upvote, downvote,date){
-        
-        this.quote = quote
-        this.author = author
-        this.upvote = upvote
-        this.downvote = downvote
-        this.date = date
-        
+    public myDate:Date;
+    constructor(public quote:string, public publisher:string, public quoteAuthor:string){
+        this.upvote=0
+        this.downvote=0
+        this.myDate=new Date()
     }
 }
